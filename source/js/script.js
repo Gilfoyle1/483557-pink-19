34e163daa2c3bdd38db2afa8fd32b395c4116461
Gilfoyle1 <kostomarov.e@gmail.com>
2020-04-28 18:41:31 +0300

    Улучшаем скорость загрузки

--- source/js/script.js
+++ source/js/script.js
@@ -19,31 +19,3 @@ navToggle.addEventListener('click', function () {
     colorHeader.classList.add("page-header__control-bgc");
   }
 });
-
-// модалки
-
-var modalSuccess = document.querySelector(".modal--success");
-var modalError = document.querySelector(".modal--error");
-var modalClose = document.querySelector(".modal__btn");
-var surname = document.querySelector("[name=surname]");
-var name = document.querySelector("[name=first-name]");
-var email = document.querySelector("[name=email]");
-var form = document.querySelector(".contest__form");
-
-
-
-form.addEventListener("submit", function (evt) {
-  if (!surname.value || !name.value || !email.value) {
-    evt.preventDefault();
-    modalError.classList.add("modal__error-show");
-  } else {
-    evt.preventDefault();
-    modalSuccess.classList.add("modal__success-show");
-  }
-});
-
-
-modalClose.addEventListener("click", function () {
-  modalError.classList.remove("modal__error-show");
-  modalSuccess.classList.remove("modal__success-show");
-});
